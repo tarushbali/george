99aa82666cd982d159eb75ccc701650c8f5e61ff
Tarush Bali <tarushbali@gmail.com>
2013-08-21 00:43:27 -0700

    Initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+George::Application.config.secret_key_base = 'd9db319a5531d82ba34987175a67a35e6160a56b95a6cde1aeeb5f075306d61fc6b2cafcf4d6980dd8a9b50ab0a6b0882a6180276175f2324fc630d131fe58d0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+George::Application.config.session_store :cookie_store, key: '_george_session'
